0398547d1860fb79e857027fe4ff9f72fad685b0
ThaissaMariana <thaissa2608mariana@gmail.com>
2024-07-11 23:49:01 -0300

    Commit - 11/07
    Implementando os domínios para o carregamento de imagens

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
 
     images:{
-        domains:["m.media-amazon.com"]
+        domains:[
+            "m.media-amazon.com",
+            "localhost:3000",
+            "rollingstone.uol.com.br"
+        ]
     }
 };
 
