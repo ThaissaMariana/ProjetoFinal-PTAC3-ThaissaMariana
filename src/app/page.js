29f744d80c65efec2407a4549f561a17246438bd
ThaissaMariana <thaissa2608mariana@gmail.com>
2024-07-11 23:30:18 -0300

    Commit - 11/07
    Configurando a página Home - Requisito #

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,7 @@
-import Image from "next/image";
-import "./styles.css";
-import Link from "next/link";
+import Main from './components/Main';
 
-export default function Home() {
-
+export default async function Home() {
   return (
-    <main>
-     
-    </main>
+    <Main />
   );
 }
